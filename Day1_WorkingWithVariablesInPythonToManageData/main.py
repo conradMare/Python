63fd7e371dcfcb4de6e8569e32adf193b8d0fcf2
conradMare <conradmare4@gmail.com>
2024-05-16 07:08:37 +0200

    2024/05/16(am)
    Day1 - S01.11_The Python Input Function

--- Day1_WorkingWithVariablesInPythonToManageData/main.py
+++ Day1_WorkingWithVariablesInPythonToManageData/main.py
@@ -1,7 +1,11 @@
-# Write your code below this line 👇
-print("Hello World!")
+# Printing to the Console in Python
+# print("Hello World!")
 
 # String Manipulation and Code Intelligence
-print("Hello World!\nHello World!\nHello World!")
+# print("Hello World!\nHello World!\nHello World!")
+# print("Hello" + " " + "Conrad")
 
-print("Hello" + " " + "Conrad")
+# The Python Input Function
+#  input() will get user input in the console
+# Then print() will print the word "Hello" and the user input
+print("Hello " + input("What is your name?") + "!")
