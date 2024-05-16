37201695811c401ec04c63577df689c530c373c1
conradMare <conradmare4@gmail.com>
2024-05-16 09:57:54 +0200

    2024/05/16(am)
    Day 2 - S02.20_Type Error, Type Checking and Type Conversion

--- Day2-UnderstandingDataTypesAndManipulatingStrings/main.py
+++ Day2-UnderstandingDataTypesAndManipulatingStrings/main.py
@@ -3,17 +3,36 @@
 # 19 - Python Primitive Data Types
 # String
 # Subscripting
-print("Hello"[4])
-
-print("123" + "345")
+# print("Hello"[4])
+#
+# print("123" + "345")
 
 # Integer
-print(123 + 345)
-123_456_789
+# print(123 + 345)
+# 123_456_789
 
 # Float
-3.14159
+# 3.14159
 
 # Boolean
-True
-False
+# True
+# False
+
+# 20 - Type Error, Type Checking and Type Conversion
+# num_char = len(input("What is your name? "))
+# # print("Your name has " + num_char + " characters.")
+
+# #Type Checking
+# print(type(num_char))
+# print(type(len(input("What is your name? "))))
+
+# #Type Conversion/Casting
+# new_num_char = str(num_char)
+# print("Your name has " + new_num_char + " characters.")
+
+a = float(123)
+print(type(a))
+
+print(70 + float("100.5"))
+
+print(str(70) + str(100))
