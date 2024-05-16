4c8e25bb130f9e2d9c5fcb7888a3f9152c6aa0cb
conradMare <conradmare4@gmail.com>
2024-05-16 09:42:21 +0200

    2024/05/16(am)
    Day 2 - S02.19_Python Primitive Data Types

---
+++ Day2-UnderstandingDataTypesAndManipulatingStrings/main.py
@@ -0,0 +1,19 @@
+# Data Types
+
+# 19 - Python Primitive Data Types
+# String
+# Subscripting
+print("Hello"[4])
+
+print("123" + "345")
+
+# Integer
+print(123 + 345)
+123_456_789
+
+# Float
+3.14159
+
+# Boolean
+True
+False
