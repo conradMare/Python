15ce076dd0a818e61b908e09315691b2c8dd9c4e
conradMare <conradmare4@gmail.com>
2024-05-16 06:51:06 +0200

    2024/05/16(am)
    Day1 - S01.09_String Manipulation and Code Intelligence

--- Day1_WorkingWithVariablesInPythonToManageData/main.py
+++ Day1_WorkingWithVariablesInPythonToManageData/main.py
@@ -1,2 +1,7 @@
 # Write your code below this line 👇
 print("Hello World!")
+
+# String Manipulation and Code Intelligence
+print("Hello World!\nHello World!\nHello World!")
+
+print("Hello" + " " + "Conrad")
