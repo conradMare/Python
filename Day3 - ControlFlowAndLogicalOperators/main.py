471dd3116950de073063b103646ffda10dac9389
conradMare <conradmare4@gmail.com>
2024-05-17 06:44:04 +0200

    2024/05/17(am)
    Day 3 - S03.37_Logical Operators

--- Day3 - ControlFlowAndLogicalOperators/main.py
+++ Day3 - ControlFlowAndLogicalOperators/main.py
@@ -24,6 +24,34 @@
 #     print("Sorry, you have to grow taller before you can ride.")
 
 # 35 - Multiple If Statements in Succession
+# print("Welcome to the rollercoaster!")
+# height = int(input("What is your height in cm? "))
+# bill = 0
+#
+# if height >= 120:
+#     print("You can ride the rollercoaster!")
+#     age = int(input("What is your age? "))
+#     if age < 12:
+#         bill = 5
+#         print("Child tickets are $5.")
+#     elif age <= 18:
+#         bill = 7
+#         print("Youth tickets are $7.")
+#     else:
+#         bill = 12
+#         print("Adult tickets are $12.")
+#
+#     wants_photo = input("Do you want a photo taken? Y or N. ")
+#     if wants_photo == "Y":
+#         # Add $3 to their bill
+#         bill += 3
+#
+#     print(f"Your final bill is ${bill}")
+#
+# else:
+#     print("Sorry, you have to grow taller before you can ride.")
+
+# 37 - Logical Operators
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
 bill = 0
@@ -37,6 +65,8 @@ if height >= 120:
     elif age <= 18:
         bill = 7
         print("Youth tickets are $7.")
+    elif age >= 45 and age <= 55:
+        print("Everything is going to be ok. Have a free ride on us!")
     else:
         bill = 12
         print("Adult tickets are $12.")
@@ -47,6 +77,3 @@ if height >= 120:
         bill += 3
 
     print(f"Your final bill is ${bill}")
-
-else:
-    print("Sorry, you have to grow taller before you can ride.")
