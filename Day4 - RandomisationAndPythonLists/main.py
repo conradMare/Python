cf0377c1650375f6393165804023144aa77c2ca9
conradMare <conradmare4@gmail.com>
2024-05-18 06:45:59 +0200

    2024/05/18(am)
    Day 4 - S04.42_Random Module

---
+++ Day4 - RandomisationAndPythonLists/main.py
@@ -0,0 +1,14 @@
+# 42 - Random Module
+import random
+import my_module
+
+# print(my_module.pi)
+
+random_integer = random.randint(1, 10)
+print(random_integer)
+
+random_float = random.random() * 5
+print(random_float)
+
+love_score = random.randint(1, 100)
+print(f"Your love score is {love_score}")
