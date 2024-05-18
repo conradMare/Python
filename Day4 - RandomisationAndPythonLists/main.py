90d4d2fa5e4de1a56de6e1cf5c9df0bb2b900223
conradMare <conradmare4@gmail.com>
2024-05-18 07:05:52 +0200

    2024/05/18(am)
    Day 4 - S04.44_Understanding the Offset and Appending Items to List

--- Day4 - RandomisationAndPythonLists/main.py
+++ Day4 - RandomisationAndPythonLists/main.py
@@ -12,3 +12,32 @@ print(random_float)
 
 love_score = random.randint(1, 100)
 print(f"Your love score is {love_score}")
+
+# 44 - Understanding the Offset and Appending Items to List
+states_of_america = [
+    "Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut",
+    "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia",
+    "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky",
+    "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois",
+    "Alabama", "Maine", "Missouri", "Arkansas", "Michigan", "Florida", "Texas",
+    "Iowa", "Wisconsin", "California", "Minnesota", "Oregon", "Kansas",
+    "West Virginia", "Nevada", "Nebraska", "Colorado", "North Dakota",
+    "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah",
+    "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"
+]
+
+# print(states_of_america[0])
+# print(states_of_america[-1])
+
+states_of_america[1] = "Pencilvania"
+
+# states_of_america.append("Angelaland")
+
+# states_of_america.extend(["Angelaland", "Jack Bauer Land"])
+
+print(states_of_america)
+
+dirty_dozen = [
+    "Strawberries", "Spinach", "Kale", "Nectarines", "Apples", "Grapes",
+    "Peaches", "Cherries", "Pears", "Tomatoes", "Celery", "Potatoes"
+]
