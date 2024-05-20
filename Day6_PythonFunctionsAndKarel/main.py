10716193943c6b35f6e369bcd73d573a575d922c
conradMare <conradmare4@gmail.com>
2024-05-20 09:09:40 +0200

    2024/05/20(am)
    Day6 - S06.60_Defining and Calling Python Functions

---
+++ Day6_PythonFunctionsAndKarel/main.py
@@ -0,0 +1,21 @@
+# 60 - Defining and Calling Python Functions
+# (from reeborg.ca - Practice)
+# def turn_around():
+#     turn_left()
+#     turn_left()
+#
+#
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+#
+# turn_left()
+# move()
+# turn_right()
+# move()
+# turn_right()
+# move()
+# turn_right()
+# move()
