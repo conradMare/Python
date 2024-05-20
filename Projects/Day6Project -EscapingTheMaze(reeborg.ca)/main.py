1237b0d2ceb9c4ae5fdc97731914ec730d6baa26
conradMare <conradmare4@gmail.com>
2024-05-20 10:46:33 +0200

    2024/05/20(am)
    Day 6 Project - Escape the Maze(reeborg.ca)

---
+++ Projects/Day6Project -EscapingTheMaze(reeborg.ca)/main.py
@@ -0,0 +1,34 @@
+# Run in Reeborg's World - Maze
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         turn_left()
+
+# Infinite Loop Solution
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+while front_is_clear():
+    move()
+turn_left()
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
