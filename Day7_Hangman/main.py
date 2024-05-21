245c196df8451c7e173ee0f213f2ec5102c26759
conradMare <conradmare4@gmail.com>
2024-05-21 06:53:10 +0200

    2024/05/21(am)
    Day7 - S07.71_Challenge 1 Solution - How to check the users answers

--- Day7_Hangman/main.py
+++ Day7_Hangman/main.py
@@ -7,3 +7,13 @@ word_list = ["aardvark", "baboon", "camel"]
 # TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 
 # TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+import random
+
+chosen_word = random.choice(word_list)
+guess = input("Guess a letter: ").lower()
+
+for letter in chosen_word:
+    if letter == guess:
+        print("Right")
+    else:
+        print("Wrong")
