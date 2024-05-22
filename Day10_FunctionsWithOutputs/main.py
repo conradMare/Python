f083044d035a4486d0751094c947598c4216ed6c
conradMare <conradmare4@gmail.com>
2024-05-22 08:12:47 +0200

    2024/05/22(am)
    Day10 - S10.100_Functions with Outputs

---
+++ Day10_FunctionsWithOutputs/main.py
@@ -0,0 +1,14 @@
+# 100 - Functions with Outputs
+# Functions with Outputs
+
+def format_name(f_name, l_name):
+    formatted_f_name = f_name.title()
+    formatted_l_name = l_name.title()
+
+    return f"{formatted_f_name} {formatted_l_name}"
+
+
+# formatted_string = format_name("AnGelA", "YU")
+# print(formatted_string)
+
+print(format_name("AnGelA", "YU"))
