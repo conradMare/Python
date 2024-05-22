72dddf7d28e3ff603ef23efdd6dd6e8bc1ca4dd3
conradMare <conradmare4@gmail.com>
2024-05-22 04:57:16 +0200

    2024/05/22(am)
    Day8 - S08.82_Functions with Inputs

---
+++ Day8_FunctionParametersAndCaesarCipher/main.py
@@ -0,0 +1,20 @@
+# 82 - Functions with Inputs
+# Review:
+# Create a function called greet().
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+# def greet():
+#     print("Hello")
+#     print("How are you?")
+#     print("Good Bye")
+
+# greet()
+
+# Function that allows for input
+def greet_with_name(name):
+    print(f"Hello {name}")
+    print(f"How are you {name}?")
+
+
+greet_with_name("Conrad")
