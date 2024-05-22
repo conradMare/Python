5f2caee21cbbac44e245ba64a32dc976149fc417
conradMare <conradmare4@gmail.com>
2024-05-22 08:17:50 +0200

    2024/05/22(am)
    Day10 - S10.101_Multiple return values

--- Day10_FunctionsWithOutputs/main.py
+++ Day10_FunctionsWithOutputs/main.py
@@ -1,14 +1,26 @@
-# 100 - Functions with Outputs
-# Functions with Outputs
+# # 100 - Functions with Outputs
+# # Functions with Outputs
+#
+# def format_name(f_name, l_name):
+#     formatted_f_name = f_name.title()
+#     formatted_l_name = l_name.title()
+#
+#     return f"{formatted_f_name} {formatted_l_name}"
+#
+#
+# # formatted_string = format_name("AnGelA", "YU")
+# # print(formatted_string)
+#
+# print(format_name("AnGelA", "YU"))
 
+# 101 - Multiple return values
 def format_name(f_name, l_name):
+    if f_name == "" or l_name == "":
+        return "You didn't provide valid inputs."
     formatted_f_name = f_name.title()
     formatted_l_name = l_name.title()
 
-    return f"{formatted_f_name} {formatted_l_name}"
+    return f"Result: {formatted_f_name} {formatted_l_name}"
 
 
-# formatted_string = format_name("AnGelA", "YU")
-# print(formatted_string)
-
-print(format_name("AnGelA", "YU"))
+print(format_name(input("What is your first name? "), input("What is your last name? ")))
