cd2337f7cdb5ea1cd76239d03d2a3e950420d744
conradMare <conradmare4@gmail.com>
2024-05-22 05:47:33 +0200

    2024/05/22(am)
    Day8 - S08.86_Caesar Cipher Part 1 - Encryption

--- Day8_FunctionParametersAndCaesarCipher/main.py
+++ Day8_FunctionParametersAndCaesarCipher/main.py
@@ -20,15 +20,47 @@
 # greet_with_name("Conrad")
 
 # 83 - Positional Vs. Keyword Arguments
-# Functions with more than 1 input
-def greet_with(name, location):
-    print(f"Hello {name}")
-    print(f"What is it like in {location}?")
+# # Functions with more than 1 input
+# def greet_with(name, location):
+#     print(f"Hello {name}")
+#     print(f"What is it like in {location}?")
+#
+#
+# # greet_with("Jack Bauer", "Nowhere")
+# # vs.
+# # greet_with("Nowhere", "Jack Bauer")
+#
+# # Functions with keyword arguments
+# greet_with(location="London", name="Angela")
+
+# 86 - Caesar Cipher Part 1 - Encryption
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+            'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
 
 
-# greet_with("Jack Bauer", "Nowhere")
-# vs.
-# greet_with("Nowhere", "Jack Bauer")
+# Don't change the code above 👆
 
-# Functions with keyword arguments
-greet_with(location="London", name="Angela")
+# TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+def encrypt(plain_text, shift_amount):
+    # TODO-2: Inside the encrypt function, shift each letter of the text forwards in the alphabet by the shift amount and print the encrypted text.
+    # e.g.
+    # plain_text = "hello"
+    # shift = 5
+    # cipher_text = "mjqqt"
+    # print output: "The encoded text is mjqqt"
+    cipher_text = ""
+    for letter in plain_text:
+        position = alphabet.index(letter)
+        new_position = position + shift_amount
+        new_letter = alphabet[new_position]
+        cipher_text += new_letter
+    print(f"The encoded text is {cipher_text}")
+
+
+# TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message.
+encrypt(plain_text=text, shift_amount=shift)
