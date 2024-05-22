7857b57ade0f26dcfeec76c22f61ac88f7bc81de
conradMare <conradmare4@gmail.com>
2024-05-22 07:32:08 +0200

    2024/05/22(am)
    Day9 - S09.97_The Secret Auction Program Instructions and Flow Chart

--- Day9_DictionariesNestingAndTheSecretAuction/main.py
+++ Day9_DictionariesNestingAndTheSecretAuction/main.py
@@ -27,38 +27,42 @@
 #     print(key)
 #     print(programming_dictionary[key])
 
-# 94 - Nesting Lists and Dictionaries
-# Nesting
-capitals = {
-    "France": "Paris",
-    "Germany": "Berlin",
-}
+# # 94 - Nesting Lists and Dictionaries
+# # Nesting
+# capitals = {
+#     "France": "Paris",
+#     "Germany": "Berlin",
+# }
+#
+# # Nesting a List in a Dictionary
+#
+# travel_log = {
+#     "France": ["Paris", "Lille", "Dijon"],
+#     "Germany": ["Berlin", "Hamburg", "Stuttgart"],
+# }
+#
+# # Nesting Dictionary in a Dictionary
+#
+# travel_log = {
+#     "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
+#     "Germany": {"cities_visited": ["Berlin", "Hamburg", "Stuttgart"], "total_visits": 5},
+# }
+#
+# # Nesting Dictionaries in Lists
+#
+# travel_log = [
+#     {
+#         "country": "France",
+#         "cities_visited": ["Paris", "Lille", "Dijon"],
+#         "total_visits": 12,
+#     },
+#     {
+#         "country": "Germany",
+#         "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+#         "total_visits": 5,
+#     },
+# ]
 
-# Nesting a List in a Dictionary
-
-travel_log = {
-    "France": ["Paris", "Lille", "Dijon"],
-    "Germany": ["Berlin", "Hamburg", "Stuttgart"],
-}
-
-# Nesting Dictionary in a Dictionary
-
-travel_log = {
-    "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
-    "Germany": {"cities_visited": ["Berlin", "Hamburg", "Stuttgart"], "total_visits": 5},
-}
-
-# Nesting Dictionaries in Lists
-
-travel_log = [
-    {
-        "country": "France",
-        "cities_visited": ["Paris", "Lille", "Dijon"],
-        "total_visits": 12,
-    },
-    {
-        "country": "Germany",
-        "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
-        "total_visits": 5,
-    },
-]
+# 96 - The Secret Auction Program Instructions and Flow Chart
+from replit import clear
+# HINT: You can call clear() to clear the output in the console.
