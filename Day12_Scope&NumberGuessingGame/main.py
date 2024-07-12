a675e90cc4e3a9ddad85e19d56c3bb70017ab05e
conradMare <158014474+conradMare@users.noreply.github.com>
2024-07-12 06:58:26 +0200

    2024/07/12(am)
    S12.120_How to Modify a Global Variable

--- Day12_Scope&NumberGuessingGame/main.py
+++ Day12_Scope&NumberGuessingGame/main.py
@@ -20,7 +20,6 @@
 # Global Scope
 # player_health = 10
 
-
 # def game():
 
 #     def drink_potion():
@@ -29,15 +28,27 @@
 
 #     drink_potion()
 
-
 # print(player_health)
 
 # There is no Block Scope
-game_level = 3
+# game_level = 3
+# def create_enemy():
+#   enemies = ["Skeleton", "Zombie", "Alien"]
+#   if game_level < 5:
+#       new_enemy = enemies[0]
 
-def create_enemy():
-    enemies = ["Skeleton", "Zombie", "Alien"]
-    if game_level < 5:
-        new_enemy = enemies[0]
+#   print(new_enemy)
 
-    print(new_enemy)
+# Modifying Global Scope
+enemies = 1
+
+
+def increase_enemies():
+    # global enemies
+    # enemies += 1
+    print(f"enemies inside function: {enemies}")
+    return enemies + 1
+
+
+enemies = increase_enemies()
+print(f"enemies outside function: {enemies}")
