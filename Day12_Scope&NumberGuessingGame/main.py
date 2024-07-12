b901767aec04a6992219040cdd854a5653f0d87e
conradMare <158014474+conradMare@users.noreply.github.com>
2024-07-12 07:06:40 +0200

    2024/07/12(am)
    S12.121_Python Constants and Global Constants

--- Day12_Scope&NumberGuessingGame/main.py
+++ Day12_Scope&NumberGuessingGame/main.py
@@ -40,15 +40,21 @@
 #   print(new_enemy)
 
 # Modifying Global Scope
-enemies = 1
+# enemies = 1
+
+# def increase_enemies():
+#     # global enemies
+#     # enemies += 1
+#     print(f"enemies inside function: {enemies}")
+#     return enemies + 1
+
+# enemies = increase_enemies()
+# print(f"enemies outside function: {enemies}")
+
+# Global Constants
+PI = 3.14159
+URL = "https://www.google.com"
 
 
-def increase_enemies():
-    # global enemies
-    # enemies += 1
-    print(f"enemies inside function: {enemies}")
-    return enemies + 1
-
-
-enemies = increase_enemies()
-print(f"enemies outside function: {enemies}")
+def calc():
+    print(PI)
