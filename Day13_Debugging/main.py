48b4e4e04690c1d1b11a2d89ae0559559de3c5aa
conradMare <158014474+conradMare@users.noreply.github.com>
2024-07-13 06:12:55 +0200

    2024/07/13(am)
    S13.129_Squash bugs with a print() Statement

--- Day13_Debugging/main.py
+++ Day13_Debugging/main.py
@@ -21,17 +21,19 @@
 #     print("You are a Gen Z.")
 
 # # Fix the Errors
-age = int(input("How old are you?"))
-if age > 18:
-    print(f"You can drive at age {age}.")
+# age = int(input("How old are you?"))
+# if age > 18:
+#     print(f"You can drive at age {age}.")
 
 # #Print is Your Friend
-# pages = 0
-# word_per_page = 0
-# pages = int(input("Number of pages: "))
-# word_per_page == int(input("Number of words per page: "))
-# total_words = pages * word_per_page
-# print(total_words)
+pages = 0
+word_per_page = 0
+pages = int(input("Number of pages: "))
+print(pages)
+word_per_page = int(input("Number of words per page: "))
+print(word_per_page)
+total_words = pages * word_per_page
+print(total_words)
 
 # #Use a Debugger
 # def mutate(a_list):
@@ -42,3 +44,4 @@ if age > 18:
 #   print(b_list)
 
 # mutate([1,2,3,5,8,13])
+
