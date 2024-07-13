5057d7cf78ca0a3ec9b49d0b8efbf9acbaae734a
conradMare <158014474+conradMare@users.noreply.github.com>
2024-07-13 05:43:19 +0200

    2024/07/13(am)
    S13.126_Reproduce the Bug

--- Day13_Debugging/main.py
+++ Day13_Debugging/main.py
@@ -1,25 +1,24 @@
 ############DEBUGGING#####################
 
-
 # # Describe Problem
-def my_function():
-    for i in range(1, 21):
-        if i == 20:
-            print("You got it")
-
-
-my_function()
+# def my_function():
+#     for i in range(1, 21):
+#         if i == 20:
+#             print("You got it")
+# my_function()
 
 # # Reproduce the Bug
-# from random import randint
-# dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
-# dice_num = randint(1, 6)
-# print(dice_imgs[dice_num])
+from random import randint
+
+dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
+dice_num = randint(0, 5)
+
+print(dice_imgs[dice_num])
 
 # # Play Computer
 # year = int(input("What's your year of birth?"))
 # if year > 1980 and year < 1994:
-#   print("You are a millenial.")
+#   print("You are a millennial.")
 # elif year > 1994:
 #   print("You are a Gen Z.")
 
@@ -45,3 +44,4 @@ my_function()
 #   print(b_list)
 
 # mutate([1,2,3,5,8,13])
+
