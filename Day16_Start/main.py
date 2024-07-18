f5108042cbb4e12d9261c7500cf06051a7cfbaa3
conradMare <158014474+conradMare@users.noreply.github.com>
2024-07-18 05:55:12 +0200

    2024/07/18(am)
    S16.150_How to Add Python Packages and use PyPi

--- Day16_Start/main.py
+++ Day16_Start/main.py
@@ -2,14 +2,16 @@
 #
 # print(another_module.another_variable)
 # import turtle
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
+#
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("DarkSeaGreen4")
+# timmy.forward(100)
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
 
-timmy = Turtle()
-print(timmy)
-timmy.shape("turtle")
-timmy.color("DarkSeaGreen4")
-timmy.forward(100)
-
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+# import prettytable
