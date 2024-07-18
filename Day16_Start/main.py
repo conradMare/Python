af1aa0ef58e678414e3ac228dbf9662b76ac6292
conradMare <158014474+conradMare@users.noreply.github.com>
2024-07-18 05:38:16 +0200

    2024/07/18(am)
    S16.149_Constructing Objects and Accessing their Attributes and Methods

---
+++ Day16_Start/main.py
@@ -0,0 +1,15 @@
+# import another_module
+#
+# print(another_module.another_variable)
+# import turtle
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("DarkSeaGreen4")
+timmy.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
