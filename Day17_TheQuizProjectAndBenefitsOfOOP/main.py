965961cf2592c80005cfe60c85e8d3a686fecf58
conradMare <158014474+conradMare@users.noreply.github.com>
2024-07-18 08:25:39 +0200

    2024/07/18(am)
    S17.160_Quiz Project 2: Creating the List of Question Objects from the Data

--- Day17_TheQuizProjectAndBenefitsOfOOP/main.py
+++ Day17_TheQuizProjectAndBenefitsOfOOP/main.py
@@ -23,3 +23,14 @@
 
 
 # The Quiz Game:
+from question_model import Question
+from data import question_data
+
+question_bank = []
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+print(question_bank[0].answer)
