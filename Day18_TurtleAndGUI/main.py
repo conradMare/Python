a3db290fc2831ab6df640c50501abed008bc75b5
conradMare <158014474+conradMare@users.noreply.github.com>
2024-07-19 06:17:20 +0200

    2024/07/19(am)
    S18.169_Importing Modules, Installing Packages, and Working with Aliases

--- Day18_TurtleAndGUI/main.py
+++ Day18_TurtleAndGUI/main.py
@@ -1,12 +1,18 @@
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
+#
+# tim = Turtle()
+# tim.shape("turtle")
+# tim.color("LightGreen")
+#
+# for _ in range(4):
+#     tim.forward(100)
+#     tim.left(90)
+#
+# screen = Screen()
+# screen.exitonclick()
 
-tim = Turtle()
-tim.shape("turtle")
-tim.color("LightGreen")
+import turtle as t
+tim = t.Turtle()
 
-for _ in range(4):
-    tim.forward(100)
-    tim.left(90)
-
-screen = Screen()
-screen.exitonclick()
+import heroes
+print(heroes.gen())
