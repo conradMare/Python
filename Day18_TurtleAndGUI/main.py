2a1157917661302506e3880c8bc8bd8ecb109e21
conradMare <158014474+conradMare@users.noreply.github.com>
2024-07-19 06:51:17 +0200

    2024/07/19(am)
    S18.171_Turtle Challenge 3 - Drawing Different Shapes

--- Day18_TurtleAndGUI/main.py
+++ Day18_TurtleAndGUI/main.py
@@ -17,24 +17,45 @@
 # import heroes
 # print(heroes.gen())
 
+# from turtle import Turtle, Screen
+#
+# tim = Turtle()
+# tim.shape("turtle")
+# tim.color("LightGreen")
+#
+# # for _ in range(10):
+# #     tim.forward(10)
+# #     tim.color("white")
+# #     tim.forward(10)
+# #     tim.color("LightGreen")
+#
+# for _ in range(15):
+#     tim.forward(10)
+#     tim.penup()
+#     tim.forward(10)
+#     tim.pendown()
+#
+# screen = Screen()
+# screen.exitonclick()
+
 from turtle import Turtle, Screen
+import random
 
 tim = Turtle()
 tim.shape("turtle")
 tim.color("LightGreen")
 
-# for _ in range(10):
-#     tim.forward(10)
-#     tim.color("white")
-#     tim.forward(10)
-#     tim.color("LightGreen")
+colors = ["red", "orange", "yellow", "green", "blue", "violet"]
+def draw_shape(num_sides):
+    for _ in range(num_sides):
+        angle = 360 / num_sides
+        tim.forward(100)
+        tim.left(angle)
 
-for _ in range(15):
-    tim.forward(10)
-    tim.penup()
-    tim.forward(10)
-    tim.pendown()
+
+for shape_side_n in range(3, 11):
+    tim.color(random.choice(colors))
+    draw_shape(shape_side_n)
 
 screen = Screen()
 screen.exitonclick()
-
