880538ba51036c8fb0fbd0b64ce6998a611c8437
conradMare <158014474+conradMare@users.noreply.github.com>
2024-07-19 07:17:34 +0200

    2024/07/19(am)
    S18.173_Python Tuples and How to Generate Random RGB Colors

--- Day18_TurtleAndGUI/main.py
+++ Day18_TurtleAndGUI/main.py
@@ -63,22 +63,47 @@
 # screen.exitonclick()
 
 # Draw a random walk:
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
+# import random
+#
+# tim = Turtle()
+# tim.shape("turtle")
+# tim.color("LightGreen")
+#
+# colors = ["aquamarine", "coral", "CornflowerBlue", "DarkCyan", "DarkSlateGray", "DarkSeaGreen4"]
+# directions = [0, 90, 180, 270]
+# tim.pensize(15)
+# tim.speed("fastest")
+#
+# for _ in range(200):
+#     tim.color(random.choice(colors))
+#     tim.forward(30)
+#     tim.setheading(random.choice(directions))
+#
+# screen = Screen()
+# screen.exitonclick()
+
+# Draw with random colors:
+import turtle as t
 import random
 
-tim = Turtle()
+tim = t.Turtle()
+t.colormode(255)
 tim.shape("turtle")
 tim.color("LightGreen")
 
-colors = ["aquamarine", "coral", "CornflowerBlue", "DarkCyan", "DarkSlateGray", "DarkSeaGreen4"]
+def random_color():
+    r = random.randint(0,255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_color = (r,g,b)
+    return random_color
+
 directions = [0, 90, 180, 270]
 tim.pensize(15)
-tim.speed("fastest")
+tim.speed("slowest")
 
 for _ in range(200):
-    tim.color(random.choice(colors))
+    tim.color(random_color())
     tim.forward(30)
     tim.setheading(random.choice(directions))
-
-screen = Screen()
-screen.exitonclick()
