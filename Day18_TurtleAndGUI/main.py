357b1e37422ab697f251b9353329de22d657f01c
conradMare <158014474+conradMare@users.noreply.github.com>
2024-07-19 06:27:01 +0200

    2024/07/19(am)
    S18.170_Turtle Challenge 2 - Draw a Dashed Line

--- Day18_TurtleAndGUI/main.py
+++ Day18_TurtleAndGUI/main.py
@@ -11,8 +11,30 @@
 # screen = Screen()
 # screen.exitonclick()
 
-import turtle as t
-tim = t.Turtle()
+# import turtle as t
+# tim = t.Turtle()
+#
+# import heroes
+# print(heroes.gen())
 
-import heroes
-print(heroes.gen())
+from turtle import Turtle, Screen
+
+tim = Turtle()
+tim.shape("turtle")
+tim.color("LightGreen")
+
+# for _ in range(10):
+#     tim.forward(10)
+#     tim.color("white")
+#     tim.forward(10)
+#     tim.color("LightGreen")
+
+for _ in range(15):
+    tim.forward(10)
+    tim.penup()
+    tim.forward(10)
+    tim.pendown()
+
+screen = Screen()
+screen.exitonclick()
+
