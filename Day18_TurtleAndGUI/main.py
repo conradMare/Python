5cb3cd4c3994f2b17575ab633d77e05e44c9cda9
conradMare <158014474+conradMare@users.noreply.github.com>
2024-07-19 07:32:03 +0200

    2024/07/19(am)
    S18.174_Turle Challenge 5 - Draw a Spirograph

--- Day18_TurtleAndGUI/main.py
+++ Day18_TurtleAndGUI/main.py
@@ -84,6 +84,31 @@
 # screen.exitonclick()
 
 # Draw with random colors:
+# import turtle as t
+# import random
+#
+# tim = t.Turtle()
+# t.colormode(255)
+# tim.shape("turtle")
+# tim.color("LightGreen")
+#
+# def random_color():
+#     r = random.randint(0,255)
+#     g = random.randint(0, 255)
+#     b = random.randint(0, 255)
+#     random_color = (r,g,b)
+#     return random_color
+#
+# directions = [0, 90, 180, 270]
+# tim.pensize(15)
+# tim.speed("slowest")
+#
+# for _ in range(200):
+#     tim.color(random_color())
+#     tim.forward(30)
+#     tim.setheading(random.choice(directions))
+
+# Draw a Spirograph:
 import turtle as t
 import random
 
@@ -92,18 +117,26 @@ t.colormode(255)
 tim.shape("turtle")
 tim.color("LightGreen")
 
+
 def random_color():
-    r = random.randint(0,255)
+    r = random.randint(0, 255)
     g = random.randint(0, 255)
     b = random.randint(0, 255)
-    random_color = (r,g,b)
-    return random_color
+    color = (r, g, b)
+    return color
 
-directions = [0, 90, 180, 270]
-tim.pensize(15)
-tim.speed("slowest")
 
-for _ in range(200):
-    tim.color(random_color())
-    tim.forward(30)
-    tim.setheading(random.choice(directions))
+tim.speed("fastest")
+
+
+def draw_spirograph(size_of_gap):
+    for _ in range(int(360 / size_of_gap)):
+        tim.color(random_color())
+        tim.circle(100)
+        tim.setheading(tim.heading() + size_of_gap)
+
+
+draw_spirograph(5)
+
+screen = t.Screen()
+screen.exitonclick()
