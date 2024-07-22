d08a6b274fb0fa0b3168847a14236d55334442c5
conradMare <158014474+conradMare@users.noreply.github.com>
2024-07-22 08:00:41 +0200

    2024/07/22(am)
    S21.197_How to Slice Lists & Tuples in Python

---
+++ Day21_SnakeGamePart2.InheritanceAndListSlicing/Slicing.py
@@ -0,0 +1,16 @@
+piano_keys = ["a", "b", "c", "d", "e", "f", "g"]
+piano_tuple = ("do", "re", "mi", "fa", "so", "la", "ti")
+
+# print(piano_tuple[2:5])
+# print(piano_tuple[1:])
+
+# Setting the increment to slice:
+# print(piano_keys[2:5:2])
+
+# Slicing everything in the list, but with an increment of 2:
+# print(piano_keys[::2])
+
+# Slicing the list in reverse:
+# print(piano_keys[::-1])
+
+# Slicing also works with tuples
