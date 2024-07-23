bf16a056ab3ae1960453277424c16773f4bb368f
conradMare <158014474+conradMare@users.noreply.github.com>
2024-07-23 05:11:35 +0200

    2024/07/23(am)
    S22.206_How to Detect when the Ball goes Out of Bounds

--- Day22_BuildPong.TheFamousArcadeGame/main.py
+++ Day22_BuildPong.TheFamousArcadeGame/main.py
@@ -33,4 +33,12 @@ while game_is_on:
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
 
+    # Detect R Paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+
+    # Detect L Paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
+
 screen.exitonclick()
