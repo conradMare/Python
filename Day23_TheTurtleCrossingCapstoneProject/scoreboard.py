745f0c4d42834ba2f23d6effac06dd6faccc62c8
conradMare <158014474+conradMare@users.noreply.github.com>
2024-07-24 06:27:32 +0200

    2024/07/24(am)
    S23.218_Step7 - Add the Scoreboard and Game Over sequence

--- Day23_TheTurtleCrossingCapstoneProject/scoreboard.py
+++ Day23_TheTurtleCrossingCapstoneProject/scoreboard.py
@@ -1,5 +1,27 @@
+from turtle import Turtle
+
 FONT = ("Courier", 24, "normal")
+ALIGNMENT = "left"
+GAME_OVER_ALIGNMENT = "center"
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.level = 1
+        self.hideturtle()
+        self.penup()
+        self.goto(-280, 250)
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Level: {self.level}", align=ALIGNMENT, font=FONT)
+
+    def increase_level(self):
+        self.level += 1
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(f"GAME OVER", align=GAME_OVER_ALIGNMENT, font=FONT)
