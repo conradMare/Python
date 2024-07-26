ec0e438a56ac2893ce1a77d2269c77c77adefc37
conradMare <158014474+conradMare@users.noreply.github.com>
2024-07-26 06:33:55 +0200

    2024/07/26(am)
    S24.224_Understand Relative and Absolute File Paths

--- Day24_OpenReadWriteToFile/main.py
+++ Day24_OpenReadWriteToFile/main.py
@@ -1,9 +1,21 @@
-with open("my_file.txt") as file:
-    contents = file.read()
-    print(contents)
+# with open("my_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
 
+# S24.222_How to Open, Read and Write to Files using the "with" Keyword:
 # with open("my_file.txt", mode="a") as file:
 #     file.write("\nNew text.")
 
-with open("new_file.txt", mode="w") as file:
-    file.write("New text.")
+# with open("new_file.txt", mode="w") as file:
+#     file.write("New text.")
+
+# S24.224_Understand Relative and Absolute File Paths:
+# Example if file was moved to Desktop using Absolute Path: (Video example)
+# with open("/Users/LondonAppBrewery/Desktop/my_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
+
+# Example if file was moved to Desktop using Relative Path: (Video Example)
+with open("../../Desktop/my_file.txt") as file:
+    contents = file.read()
+    print(contents)
